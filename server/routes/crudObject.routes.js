7fe46111af0ad581289b525b69d51f895c196000
David Steed <davidasteed@gmail.com>
2017-06-28 17:51:14 -0400

    node running, connected to db, and mongodb database running

--- server/routes/crudObject.routes.js
+++ server/routes/crudObject.routes.js
@@ -1,2 +1,15 @@
-const objectRouter = require('express').Router();
+const crudObjectRouter = require('express').Router();
 const CrudObject = require('../models/CrudObject.model.js');
+
+function createCrudObject(request, response, next) {
+  if(!request.body || Object.keys(request.body).length === 0) {
+    let err = new Error('You must provide an object');
+    err.status = 400;
+    next(err);
+    return;
+  }
+  console.log('createCrudObject() was called');
+}
+
+crudObjectRouter.post('/', createCrudObject);
+module.exports = crudObjectRouter;
